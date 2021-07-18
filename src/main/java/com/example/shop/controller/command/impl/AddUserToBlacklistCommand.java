5bc3d97b2d68e00195b7363f46beba0bf128d8e3
lexo4kaa <alesha.krupenko@gmail.com>
2021-07-18 14:22:55 +0300

    fix on users_info page

--- src/main/java/com/example/shop/controller/command/impl/AddUserToBlacklistCommand.java
+++ src/main/java/com/example/shop/controller/command/impl/AddUserToBlacklistCommand.java
@@ -6,6 +6,7 @@ import com.example.shop.model.service.ServiceException;
 import com.example.shop.model.service.UserService;
 import com.example.shop.model.service.impl.UserServiceImpl;
 import com.example.shop.resource.ConfigurationManager;
+import com.example.shop.resource.MessageManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -25,19 +26,24 @@ public class AddUserToBlacklistCommand implements ActionCommand {
         HttpSession session = request.getSession();
         try {
             String banReason = request.getParameter(PARAM_NAME_BAN_REASON);
-            String stringUserId = request.getParameter(PARAM_NAME_USER_ID);
-            int userId = Integer.parseInt(stringUserId);
-            userService.addUserToBlacklist(userId, banReason);
-            List<User> users = (List<User>) session.getAttribute("users");
-            for(int i = 0; i < users.size(); i++) {
-                User user = users.get(i);
-                if(user.getUserId() == userId) {
-                    user.setIsBanned(true);
-                    users.set(i, user);
-                    break;
+            int userId = Integer.valueOf(request.getParameter(PARAM_NAME_USER_ID));
+            String nickname = (String) session.getAttribute("nickname");
+            User activeUser = userService.findUserByNickname(nickname);
+            if(activeUser.getUserId() != userId) {
+                userService.addUserToBlacklist(userId, banReason);
+                List<User> users = (List<User>) session.getAttribute("users");
+                for (int i = 0; i < users.size(); i++) {
+                    User user = users.get(i);
+                    if (user.getUserId() == userId) {
+                        user.setIsBanned(true);
+                        users.set(i, user);
+                        break;
+                    }
                 }
+                session.setAttribute("users", users);
+            } else {
+                request.setAttribute("actOnYourselfMessage", MessageManager.getProperty("message.actonyourself"));
             }
-            session.setAttribute("users", users);
             page = (String) session.getAttribute("currentPage");
         } catch (ServiceException e) {
             logger.info("Problems in 'AddUserToBlacklistCommand', redirected to error page");
